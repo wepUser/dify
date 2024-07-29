79c1bdd2b274ef0118098cd3fb277e418c75cbf8
lishuping <15230612808@163.com>
2024-07-29 02:53:26 +0000

    fix: 修复文档上传回显语法报错问题

--- web/context/app-context.tsx
+++ web/context/app-context.tsx
@@ -96,10 +96,10 @@ export const AppContextProvider: FC<AppContextProviderProps> = ({ children }) =>
     if (userProfileResponse && !userProfileResponse.bodyUsed) {
       const result = await userProfileResponse.json()
       setUserProfile(result)
-      const current_version = userProfileResponse.headers.get('x-version')
+      const current_version = process.env.NEXT_PUBLIC_APP_VERSION || userProfileResponse.headers.get('x-version')
       const current_env = process.env.NODE_ENV === 'development' ? 'DEVELOPMENT' : userProfileResponse.headers.get('x-env')
       const versionData = await fetchLanggeniusVersion({ url: '/version', params: { current_version } })
-      setLangeniusVersionInfo({ ...versionData, current_version: '0.1.0', latest_version: versionData.version, current_env })
+      setLangeniusVersionInfo({ ...versionData, current_version, latest_version: versionData.version, current_env })
     }
   }, [userProfileResponse])
 
